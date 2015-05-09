511b24cc320a37fbbbab462939524f0cce51c92b
DaM <kontakt@dpisarczyk.pl>
2015-05-10 00:41:02 +0200

    fixes and dvbt support

--- default.py
+++ default.py
@@ -11,65 +11,76 @@ class Player( xbmc.Player ):
 			getInfo(self)
 
 def getInfo(player):
-	
+
 	fileName = ""
 
 	while(player.isPlaying()):
-		
-		writeEcmInfo(parseEcmInfo())
+
+		writeEcmInfo(True)
 
 		if(player.isPlaying() and fileName != player.getPlayingFile()):
 			fileName = player.getPlayingFile()
 			channelNumber = xbmc.getInfoLabel('VideoPlayer.ChannelNumber')
-			writeTpInfo(parseTpInfo(channelNumber))
+			writeTpInfo(channelNumber)
 
 		xbmc.sleep(100)
-	
-	writeEcmInfo({"caid": "", "source": "", "hops": "", "time": ""});
-	writeTpInfo({'frequency': "", 'position': "", 'position_name': "", 'modulation': "", 'fec': "", 'system': "", 'symbolrate': "", 'polarization': ""})
+
+	writeEcmInfo(None)
+	writeTpInfo(None)
 
 def parseEcmInfo():
 
 	try:
-		ecminfo = open('/tmp/ecm.info').read()
+		tmp_ecminfo = open('/tmp/ecm.info').read()
 
-		caid = re.findall('caid: (0x.+)', ecminfo)[0]
-		source = re.findall('reader: (.+)', ecminfo)[0]
-		hops = re.findall('hops: ([0-9]+)', ecminfo)[0]
-		time = re.findall('ecm time: ([0-9.]+)', ecminfo)[0]
+		ecminfo = {
+			'caid': re.findall('caid: (0x.+)', tmp_ecminfo)[0],
+			'source': re.findall('reader: (.+)', tmp_ecminfo)[0],
+			'hops': re.findall('hops: ([0-9]+)', tmp_ecminfo)[0],
+			'time': re.findall('ecm time: ([0-9.]+)', tmp_ecminfo)[0]
+		}
 	except:
-		caid = ""
-		source = ""
-		hops = ""
-		time = ""
-		
-	return {"caid": caid, "source": source, "hops": hops, "time": time}
+		ecminfo = None
 
-def writeEcmInfo(ecminfo):
+	return ecminfo
+
+def writeEcmInfo(write):
+	
+	defaults = {
+		'caid': "",
+		'source': "",
+		'hops': "",
+		'time': ""
+	}
+
+	ecminfo = parseEcmInfo()
+	
+	if(write is None or ecminfo is None):
+		return defaults
+	
 	xbmcgui.Window(10000).setProperty('PVR_ECM_caid', ecminfo['caid'])
 	xbmcgui.Window(10000).setProperty('PVR_ECM_from', ecminfo['source'])
 	xbmcgui.Window(10000).setProperty('PVR_ECM_hops', ecminfo['hops'])
 	xbmcgui.Window(10000).setProperty('PVR_ECM_time', ecminfo['time'])
 
-def parseTpInfo(channelNumber):
+def writeTpInfo(channelNumber):
+	
+	defaults = {
+		'frequency': "",
+		'position': "",
+		'position_name': "",
+		'modulation': "",
+		'fec': "",
+		'system': "",
+		'symbolrate': "",
+		'polarization': ""
+	}
 
-	try:
-		tpinfo = channels[int(channelNumber)]
-	except KeyError:
-		tpinfo = {
-			'frequency': "",
-			'position': "",
-			'position_name': "",
-			'modulation': "",
-			'fec': "",
-			'system': "",
-			'symbolrate': "",
-			'polarization': ""
-		}
+	tpinfo = getTpInfo(channelNumber)
 
-	return tpinfo
-
-def writeTpInfo(tpinfo):
+	if(channelNumber is None or tpinfo is None):
+		return defaults
+	
 	xbmcgui.Window(10000).setProperty('PVR_SatellitePos', tpinfo['position'])
 	xbmcgui.Window(10000).setProperty('PVR_SatelliteName', tpinfo['position_name'])
 	xbmcgui.Window(10000).setProperty('PVR_Modulation', tpinfo['modulation'])
@@ -79,8 +90,17 @@ def writeTpInfo(tpinfo):
 	xbmcgui.Window(10000).setProperty('PVR_System', tpinfo['system'])
 	xbmcgui.Window(10000).setProperty('PVR_Symbolrate', tpinfo['symbolrate'])
 
+def getTpInfo(channelNumber):
+
+	try:
+		tpinfo = channels[int(channelNumber)]
+	except KeyError:
+		tpinfo = None
+
+	return tpinfo
+
 def parseChannels():
-	
+
 	modulationMap = {
 		'M999': 'Auto',
 		'M2': 'QPSK',
@@ -110,45 +130,65 @@ def parseChannels():
 	
 	sources = os.popen("cat /var/lib/vdr/sources.conf | grep '^S'").read().strip()
 	sources = re.findall('S([0-9\.]+.)\s+(.+)', sources)
-	sources_map = {}
+	sourcesMap = {}
 
 	for source in sources:
-		sources_map[source[0]] = source[1]
+		sourcesMap[source[0]] = source[1]
+
+	dvbtSourcesMap = {
+		538000: 'Sucha Gora MUX 3 k.52',
+		562000: 'Sucha Gora MUX 2 k.32',
+		722000: 'Sucha Gora MUX 1 k.29'
+	}
 
 	channels = os.popen("svdrpsend LSTC | grep '^250'").read().strip()
 	parsed_channels = {}
-	
-	lines = re.findall('250.([0-9]+)[\s]{1}(.+?);(.+?):([0-9]+?):(.+?):S(.+?):([0-9]+?):', channels)
-	
+
+	lines = re.findall('250.([0-9]+)[\s]{1}(.+?);(.+?):([0-9]+?):(.+?):(T|S(.+?)):([0-9]+?):', channels)
+
 	for line in lines:
+		try:
+			number = int(line[0])
+			name = line[1]
+			provider = line[2]
+			frequency = line[3]
+			parameter = line[4]
+			symbolrate = line[7]
 
-		number = int(line[0])
-		name = line[1]
-		provider = line[2]
-		frequency = line[3]
-		parameter = line[4]
-		position = line[5]
-		symbolrate = line[6]
-		polarization = parameter[0]
-		
-		modulation = modulationMap[re.findall('M[0-9]', parameter)[0]]
-		fec = fecMap[re.findall('C[0-9]+', parameter)[0]]
-		system = systemMap[re.findall('S0|S1', parameter)[0]]
-		position_name = sources_map[position]
+			fec = fecMap[re.findall('C[0-9]+', parameter)[0]]
+			
+			if(line[5] == 'T'):
+				position = line[5]
+			
+				system = 'DVB-T'
+				position_name = dvbtSourcesMap[int(str(frequency)[:6])]
+				modulation = ""
+				polarization = ""
+			else:
+				position = line[6]
+			
+				system = systemMap[re.findall('S0|S1', parameter)[0]]
+				position_name = sourcesMap[position]
+				modulation = modulationMap[re.findall('M[0-9]', parameter)[0]]
+				polarization = parameter[0]
 
-		parsed_channels[number] = {
-			'number': number,
-			'name': name,
-			'provider': provider,
-			'frequency': frequency,
-			'position': position,
-			'position_name': position_name,
-			'fec': fec,
-			'modulation': modulation,
-			'symbolrate': symbolrate,
-			'polarization': polarization,
-			'system': system
-		}
+			parsed_channels[number] = {
+				'number': number,
+				'name': name,
+				'provider': provider,
+				'frequency': frequency,
+				'position': position,
+				'position_name': position_name,
+				'fec': fec,
+				'modulation': modulation,
+				'symbolrate': symbolrate,
+				'polarization': polarization,
+				'system': system
+			}
+			
+			print(parsed_channels[number])
+		except:
+			pass
 
 	return parsed_channels
 
@@ -156,4 +196,4 @@ player = Player()
 channels = parseChannels()
 
 while(not xbmc.abortRequested):
-    xbmc.sleep(100)
+	xbmc.sleep(100)
